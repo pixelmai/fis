66fa61b2d1cf5b3de0b64b3aa02cb8c790b4fdb8
pixelmai <cursefury@gmail.com>
2020-04-25 12:47:22 +0800

    Dashboard preparations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,50 @@
 @extends('layouts.app')
 
-  
 @section('content')
-<div class="container pt-5">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">
-          <div class="bh">Dashboard</div>
-        </div>
 
-        <div class="card-body">
-          <p>Welcome to FABLAB WIS, {{ $user->fname }}!</p>
-        </div>
-      </div>
+
+<div id="sashboard" class="pt-2 px-2">
+
+  <div class="row pb-3">
+    <div class="col-lg-12">
+      <h1 class="pt-1 pb-0">Welcome, {{ $user->fname }}!</h1>
     </div>
   </div>
+
+  <div class="row">
+    <div class="col-md-3">
+      <div class="stats card">
+        <h5>ha</h5>
+        ya
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="stats card">
+        <h5>ha</h5>
+        ha
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="stats card">
+        <h5>ha</h5>
+        ha
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="stats card">
+        <h5>ha</h5>
+        ha
+      </div>
+    </div>
+
+  </div>
+
+
 </div>
+
+
+
 @endsection
