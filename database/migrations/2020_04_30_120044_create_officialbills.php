b622f9b50a2d9ced44291db7e5fa7a88fec1c9b7
pixelmai <cursefury@gmail.com>
2020-05-01 18:30:33 +0800

    Graphics

---
+++ database/migrations/2020_04_30_120044_create_officialbills.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableOfficialbills extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('officialbills', function (Blueprint $table) {
+            $table->id();
+            $table->integer('invoice_id')->unsigned();
+            $table->string('for_name'); 
+            $table->string('for_company')->nullable();
+            $table->string('for_position')->nullable();
+            $table->text('for_address')->nullable();
+            $table->text('letter')->nullable();
+            $table->date('billing_date'); 
+            $table->string('by_name'); 
+            $table->string('by_position');
+            $table->tinyInteger('status')->default(1); 
+            $table->unsignedBigInteger('updatedby_id');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_officialbills');
+    }
+}
